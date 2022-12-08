42e1bc0f40c254ff5293734c499d81cb4ae610b8
Muhammed Safa <muhammed.safa@borealisai.com>
2022-12-08 15:55:14 +0000

    Fix unix/windows path issue

--- 2022/q7.py
+++ 2022/q7.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from pathlib import PurePath
 from collections import defaultdict
 
 
@@ -11,16 +11,14 @@ class Solution:
         result1, result2 = 0, 0
         readings = f.read().split("\n")
         fs = defaultdict(list)
-        curr = Path("/")
+        curr = PurePath("/")
         for reading in readings:
             if reading[:4] == "$ cd":
                 if reading[5:7] == "..":
                     curr = curr.parent
                 else:
                     curr = curr / reading[5:]
-            elif reading[:4] == "$ ls":
-                continue
-            elif reading[:3] != "dir":
+            elif reading[:4] not in {"dir ", "$ ls"}:
                 size, name = reading.split(" ")
                 size = int(size)
                 fs[curr].append((name, size))
@@ -30,15 +28,15 @@ class Solution:
             _sum = 0
             for name, size in fs[p]:
                 _sum += size
-            while p != Path("\\"):
+            while p != PurePath("/"):
                 sizes[p] = sizes.get(p, 0) + _sum
                 p = p.parent
             sizes[p] = sizes.get(p, 0) + _sum
 
         _max = 70000000
         required = 30000000
-        needed = required - _max + sizes[Path("\\")]
-        result2 = sizes[Path("\\")]
+        needed = required - _max + sizes[PurePath("/")]
+        result2 = sizes[PurePath("/")]
         for p in sizes:
             if sizes[p] < 100000:
                 result1 += sizes[p]
@@ -49,4 +47,4 @@ class Solution:
 
 
 solver = Solution()
-print(solver.solve("2022/q7.txt"))
+print(solver.solve("/home/msafa/adventOfCode/2022/q7.txt"))
