d72c34a24d41cdd089d321d0716c2f408729b792
Muhammed Safa <muhammed.safa@mail.mcgill.ca>
2022-12-14 23:11:24 -0500

    Improve readability

--- 2022/q14.py
+++ 2022/q14.py
@@ -5,75 +5,52 @@ class Solution:
     def __init__(self):
         pass
 
+    def get_next_pos(self, blocked, lowest):
+        x, y = 500, 0
+        while True:
+            while (x, y + 1) not in blocked and y < lowest:
+                y = y + 1
+            if (x - 1, y + 1) not in blocked and y < lowest:
+                x, y = x - 1, y + 1
+            elif (x + 1, y + 1) not in blocked and y < lowest:
+                x, y = x + 1, y + 1
+            else:
+                break
+        return (x, y)
+
     def solve(self, filepath):
         f = open(filepath, "r")
-        result1, result2 = 0, 0
+        result1, result2 = 0, 1
         readings = f.read().split("\n")
-        rocks = [
-            [[int(x) for x in r.split(",")] for r in reading.split(" -> ")]
-            for reading in readings
-        ]
         rock_pos = set()
         lowest = 0
-        for rock in rocks:
-            for i in range(1, len(rock)):
-                rock_pos.add(tuple(rock[i]))
-                rock_pos.add(tuple(rock[i - 1]))
-                lowest = max(lowest, rock[i][1])
-                lowest = max(lowest, rock[i - 1][1])
-                if rock[i][0] == rock[i - 1][0]:
-                    diff = rock[i][1] - rock[i - 1][1]
-                    while diff != 0:
-                        rock_pos.add((rock[i][0], rock[i][1] - diff))
-                        lowest = max(lowest, rock[i][1] - diff)
-                        diff += 1 if diff < 0 else -1
-                if rock[i][1] == rock[i - 1][1]:
-                    diff = rock[i][0] - rock[i - 1][0]
-                    while diff != 0:
-                        rock_pos.add((rock[i][0] - diff, rock[i][1]))
-                        diff += 1 if diff < 0 else -1
+
+        for reading in readings:
+            for i, r in enumerate(reading.split(" -> ")):
+                if i == 0:
+                    x, y = [int(x) for x in r.split(",")]
+                    continue
+                xp, yp = [int(x) for x in r.split(",")]
+                dx, dy = xp - x, yp - y
+                l = abs(dx + dy)
+                rock_pos |= {(x + i * dx // l, y + i * dy // l) for i in range(l + 1)}
+                lowest = max([lowest, y, y + dy])
+                x, y = xp, yp
 
         blocked = copy.deepcopy(rock_pos)
-        while True:
-            x, y = 500, 0
-            while True:
-                while (x, y + 1) not in blocked and y < lowest:
-                    y += 1
-                if (x - 1, y + 1) not in blocked and y < lowest:
-                    x -= 1
-                    y += 1
-                elif (x + 1, y + 1) not in blocked and y < lowest:
-                    x += 1
-                    y += 1
-                else:
-                    break
-
-            if y == lowest:
-                break
+        x, y = self.get_next_pos(blocked, lowest)
+        while y != lowest:
             blocked.add((x, y))
             result1 += 1
+            x, y = self.get_next_pos(blocked, lowest)
 
         blocked = copy.deepcopy(rock_pos)
         lowest = lowest + 1
-        while True:
-            x, y = 500, 0
-            while True:
-                while (x, y + 1) not in blocked and y < lowest:
-                    y += 1
-                if (x - 1, y + 1) not in blocked and y < lowest:
-                    x -= 1
-                    y += 1
-                elif (x + 1, y + 1) not in blocked and y < lowest:
-                    x += 1
-                    y += 1
-                else:
-                    break
-
-            if (x, y) == (500, 0):
-                result2 += 1
-                break
+        x, y = self.get_next_pos(blocked, lowest)
+        while (x, y) != (500, 0):
             blocked.add((x, y))
             result2 += 1
+            x, y = self.get_next_pos(blocked, lowest)
 
         return result1, result2
 
