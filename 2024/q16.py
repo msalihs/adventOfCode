c595642ed6a85d0c165afd008d7bc7419888d36d
Muhammed Safa <muhammed.safa@mail.mcgill.ca>
2024-12-18 23:38:21 -0500

    Fix Q16

--- 2024/q16.py
+++ 2024/q16.py
@@ -10,60 +10,41 @@ class Solution:
         f = open(filepath, "r")
         _input = f.read().split("\n")
 
-        grid = [list(line) for line in _input]
+        valid_points = {}
+        for i, line in enumerate(_input):
+            for j, char in enumerate(list(line)):
+                if char != "#":
+                    valid_points[i + j * 1j] = char
 
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == "S":
-                    start = (i, j)
-                if grid[i][j] == "E":
-                    end = (i, j)
+        (start,) = (p for p in valid_points if valid_points[p] == "S")
+        (end,) = (p for p in valid_points if valid_points[p] == "E")
 
-        queue = [(0, start[0], start[1], 0, 1)]
+        queue = [(0, 0, start, 1j, {start})]
+        costs = {}
+        seen = set()
         idx = 0
-        happens = 0
-        seen = {(start[0], start[1], 0, 1): 0}
+
         while queue:
-            idx += 1
-            cost, i, j, di, dj = heapq.heappop(queue)
+            cost, _, p, dir, path = heapq.heappop(queue)
 
-            if (
-                grid[i + di][j + dj] != "#"
-                and seen.get((i + di, j + dj, di, dj), 10 ** 10) > cost + 1
-            ):
-                heapq.heappush(queue, (cost + 1, i + di, j + dj, di, dj))
-                seen[(i + di, j + dj, di, dj)] = cost + 1
-                if (i + di, j + dj) == end:
-                    result1 = min(cost + 1, result1)
-                    happens += 1
-            if seen.get((i, j, dj, di), 10 ** 10) > cost + 1000:
-                heapq.heappush(queue, (cost + 1000, i, j, dj, di))
-                seen[(i, j, dj, di)] = cost + 1000
-            if seen.get((i, j, -dj, -di), 10 ** 10) > cost + 1000:
-                heapq.heappush(queue, (cost + 1000, i, j, -dj, -di))
-                seen[(i, j, -dj, -di)] = cost + 1000
+            if cost > costs.get((p, dir), 10 ** 10):
+                continue
 
-        queue = [end]
-        result2 += 1
-        visited = {end}
-        while queue:
-            i, j = queue.pop()
-            min_val = result1
-            for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                for ddi, ddj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                    min_val = min(
-                        min_val, seen.get((i + di, j + dj, ddi, ddj), 10 ** 10)
-                    )
+            costs[(p, dir)] = cost
 
-            for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                for ddi, ddj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                    if (
-                        seen.get((i + di, j + dj, ddi, ddj), 10 ** 10) == min_val
-                        and (i + di, j + dj) not in visited
-                    ):
-                        visited.add((i + di, j + dj))
-                        queue.append((i + di, j + dj))
-                        result2 += 1
+            if p == end and cost <= result1:
+                seen |= path
+                result1 = cost
+
+            for d, val in [(1, 1), (1j, 1001), (-1j, 1001)]:
+                costp = cost + val
+                pp = p + dir * d
+                dirp = dir * d
+                idx += 1
+                if pp in valid_points and pp not in path:
+                    heapq.heappush(queue, (costp, idx, pp, dirp, path | {pp}))
+
+        result2 = len(seen)
 
         return result1, result2
 
